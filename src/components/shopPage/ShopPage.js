75f94fccedbbfacd7dd14f5e2f131015f2d5ea4c
exionite <exionite@gmail.com>
2022-07-06 13:56:37 -0700

    carousel styling

--- src/components/shopPage/ShopPage.js
+++ src/components/shopPage/ShopPage.js
@@ -1,35 +1,12 @@
 import React from 'react';
 import './ShopPage.scss'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-// import { Carousel } from 'react-responsive-carousel';
-// import wallConnector from '../../Assets/Images/wall_connector.avif'
-// import chillBanner from '../../Assets/Images/chill_banner.avif'
-// import floorMats from '../../Assets/Images/floormats.avif'
-// import { ShopNowButton } from '../Styled';
 import HeroCarousel from './HeroCarousel';
 
 const ShopPage = () => {
   return (
     <div className='shop-page-component'>
       <HeroCarousel />
-      {/* <div className="hero-carousel"> */}
-        {/* <Carousel infiniteLoop emulateTouch interval="5000" showThumbs={false}>
-          <div className='carousel-image-container'>
-            <img src={wallConnector} alt="" />
-            <div className="carousel-text">
-              <h1>Wall Connector</h1>
-              <h2>The most convenient way to charge at home</h2>
-              <ShopNowButton>SHOP NOW</ShopNowButton>
-            </div>
-          </div>
-          <div className='carousel-image-container'>
-            <img src={chillBanner} alt="" />
-          </div>
-          <div className='carousel-image-container'>
-            <img src={floorMats} alt="" />
-          </div>
-        </Carousel> */}
-      {/* </div> */}
     </div>
   );
 }
