0b2a9f20eebcc214b2ed0b0f33874ab9923389a6
exionite <exionite@gmail.com>
2022-07-06 11:12:15 -0700

    carousel styling

--- src/components/ShopPage.js
+++ src/components/ShopPage.js
@@ -5,19 +5,25 @@ import { Carousel } from 'react-responsive-carousel';
 import wallConnector from '../Assets/Images/wall_connector.avif'
 import chillBanner from '../Assets/Images/chill_banner.avif'
 import floorMats from '../Assets/Images/floormats.avif'
+import { ShopNowButton } from './Styled';
 
 const FabiansComponent = () => {
   return (
     <div className='shop-page-component'>
       <div className="hero-carousel">
-        <Carousel infiniteLoop autoPlay emulateTouch interval="5000" showThumbs={false}>
-          <div>
+        <Carousel  infiniteLoop emulateTouch interval="5000" showThumbs={false}>
+          <div className='carousel-image-container'>
             <img src={wallConnector} alt="" />
+            <div className="carousel-text">
+              <h1>Wall Connector</h1>
+              <h2>The most convenient way to charge at home</h2>
+              <ShopNowButton>Shop Now</ShopNowButton>
+            </div>
           </div>
-          <div>
+          <div className='carousel-image-container'>
             <img src={chillBanner} alt="" />
           </div>
-          <div>
+          <div className='carousel-image-container'>
             <img src={floorMats} alt="" />
           </div>
         </Carousel>
--- src/components/Styled.js
+++ src/components/Styled.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components'
 
-export const Button = styled.button`
-border: 2px solid red;
-background-color: purple;
-border-radius: 40px;
+export const ShopNowButton = styled.button`
+border: none;
+border-radius: 50px;
+padding: 18px;
+width: 20%;
 `
