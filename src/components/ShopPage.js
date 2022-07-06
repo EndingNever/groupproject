58ca5d47319414c2e69bc4ee793c605d7f314dce
exionite <exionite@gmail.com>
2022-07-06 10:02:51 -0700

    carousel working, with images

--- src/components/ShopPage.js
+++ src/components/ShopPage.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './ShopPage.scss'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 import wallConnector from '../wall_connector.avif'
+import chillBanner from '../chill_banner.avif'
+import floorMats from '../floormats.avif'
 
 const FabiansComponent = () => {
   return (
     <div className='shop-page-component'>
       <div className="hero-carousel">
-        <img src={wallConnector} alt="" />
+        <Carousel infiniteLoop autoPlay emulateTouch interval="5000" showThumbs={false}>
+          <div>
+            <img src={wallConnector} alt="" />
+          </div>
+          <div>
+            <img src={chillBanner} alt="" />
+          </div>
+          <div>
+            <img src={floorMats} alt="" />
+          </div>
+        </Carousel>
       </div>
       {/* <h1>Test</h1> */}
     </div>
