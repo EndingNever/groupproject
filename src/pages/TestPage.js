c45f5e41f3d8e42535e5d6398e272013ea12923a
SyntheticDesigner <andrew.sch.j@gmail.com>
2022-07-06 11:12:06 -0700

    created router and setup test parameters

--- src/pages/TestPage.js
+++ src/pages/TestPage.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
+import { useParams } from "react-router-dom";
 
 const TestPage = () => {
-    return (
-        <div>
-            <h1>Test Route</h1>
-        </div>
-    );
-}
+  const { productCategory } = useParams();
+  return (
+    <div>
+      <h1>Test Route</h1>
+      <h2>{productCategory ? productCategory : "Cart"}</h2>
+    </div>
+  );
+};
 
 export default TestPage;
