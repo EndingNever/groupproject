6c60ca86dc2ff5c967fa54128aa3156498f45c52
exionite <exionite@gmail.com>
2022-07-07 11:23:23 -0700

    apparel and lifestyle photos

--- src/components/navBar/Nav.js
+++ src/components/navBar/Nav.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import TeslaLogo from '../../assets/Images/TeslaLogo'
+import TeslaLogo from '../../assets/images/TeslaLogo'
 import { useNavigate } from "react-router-dom";
 
 const StyledNav = styled.nav`
--- src/components/shopPage/ShopPage.js
+++ src/components/shopPage/ShopPage.js
@@ -3,11 +3,16 @@ import './ShopPage.scss'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import HeroCarousel from './HeroCarousel';
 import VehicleAccessories from './VehicleAccessories';
-import modelS from '../../assets/Images/modelS.avif'
-import model3 from '../../assets/Images/model3.avif'
-import modelX from '../../assets/Images/modelX.avif'
-import modelY from '../../assets/Images/modelY.avif'
-import charging from '../../assets/Images/charging.avif'
+import modelS from '../../assets/images/modelS.avif'
+import model3 from '../../assets/images/model3.avif'
+import modelX from '../../assets/images/modelX.avif'
+import modelY from '../../assets/images/modelY.avif'
+import charging from '../../assets/images/charging.avif'
+import men from '../../assets/images/men.avif'
+import women from '../../assets/images/women.avif'
+import kids from '../../assets/images/kids.avif'
+import lifestyle from '../../assets/images/lifestyle.avif'
+import { ShopNowButton } from '../Styled';
 
 const ShopPage = () => {
   return (
@@ -17,10 +22,36 @@ const ShopPage = () => {
         Tesfaye
       </div>
       <VehicleAccessories image={modelS} title={'Model S Accessories'} />
-      <VehicleAccessories image={model3} title={'Model 3 Accessories'} color={"black"}/>
+      <VehicleAccessories image={model3} title={'Model 3 Accessories'} color={"black"} />
       <VehicleAccessories image={modelX} title={'Model X Accessories'} />
-      <VehicleAccessories image={modelY} title={'Model Y Accessories'} color={"black"}/>
-      <VehicleAccessories image={charging} title={'Charging'}/>
+      <VehicleAccessories image={modelY} title={'Model Y Accessories'} color={"black"} />
+      <VehicleAccessories image={charging} title={'Charging'} />
+      <div className="apparel">
+        <div className="men">
+          <img src={men} alt="men's clothing" />
+          <div className="apparel-text">
+            <h1>Men's apparel</h1>
+            <ShopNowButton>Shop Now</ShopNowButton>
+          </div>
+        </div>
+        <div className="women">
+          <img src={women} alt="women's clothing" />
+          <div className="apparel-text">
+            <h1>Women's apparel</h1>
+            <ShopNowButton>Shop Now</ShopNowButton>
+          </div>
+        </div>
+        <div className="kids">
+          <img src={kids} alt="kid's clothing" />
+          <div className="apparel-text">
+            <h1>Kid's apparel</h1>
+            <ShopNowButton>Shop Now</ShopNowButton>
+          </div>
+        </div>
+      </div>
+      <div className="lifestyle">
+        <img src={lifestyle} alt="" />
+      </div>
     </div>
   );
 }
