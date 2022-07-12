fc80c9788131dc5b7f26c3ed2726ea6bfaae04f3
exionite <exionite@gmail.com>
2022-07-12 09:32:05 -0700

    images

--- src/Data.js
+++ src/Data.js
@@ -23,6 +23,31 @@ import modelSLiner from "./assets/images/modelSLiner.avif";
 import modelSLinerHov from "./assets/images/modelSLinerHover.avif";
 import SRoofRack from "./assets/images/SRoofRack.avif";
 import SRoofRackHov from "./assets/images/SRoofRackHov.avif";
+import modelSRearLiner from "./assets/images/modelSRearLiner.avif";
+import modelSRearLinerHov from "./assets/images/modelSRearLinerHov.avif";
+import S3YLiner from "./assets/images/S3YLiner.avif";
+import S3YLinerHov from "./assets/images/S3YLinerHov.avif";
+import SDoorSills from "./assets/images/SDoorSills.avif"
+import SDoorSillsHov from "./assets/images/SDoorSillsHov.avif"
+import SXCoatHooks from "./assets/images/SXCoatHooks.avif"
+import SXCoatHooksHov from "./assets/images/SXCoatHooksHov.avif"
+import SunShades from "./assets/images/SunShades.avif"
+import SMudFlaps from "./assets/images/SMudFlaps.avif"
+import SMudFlapsHov from "./assets/images/SMudFlapsHov.avif"
+import SCarCover from "./assets/images/SCarCover.avif"
+import SCarCoverHov from "./assets/images/SCarCoverHov.avif"
+import ModelSSpoiler from "./assets/images/ModelSSpoiler.avif"
+import ModelSSpoilerHov from "./assets/images/ModelSSpoilerHov.avif"
+import SBreakKit from "./assets/images/SBreakKit.avif"
+import STempestWheel from "./assets/images/STempestWheel.avif"
+import STempestWheelHov from "./assets/images/STempestWheelHov.avif"
+import WheelLocks from "./assets/images/WheelLocks.avif"
+import WheelLocksHov from "./assets/images/WheelLocksHov.avif"
+import ArachnidWheel from "./assets/images/ArachnidWheel.avif"
+import ArachnidWheelHov from "./assets/images/ArachnidWheelHov.avif"
+import TireTote from "./assets/images/TireTote.avif"
+import TireToteHov from "./assets/images/TireToteHov.avif"
+
 
 const items = [
   {
@@ -191,7 +216,7 @@ const items = [
             subCategory: "model-s",
             category: "model-s",
             stockStatus: true,
-            options: ["view-details", "best-seller", "interior", "floor-mats"],
+            options: ["view-details", "best-seller", "Interior", "Floor Mats"],
           },
           {
             itemImg: SRoofRack,
@@ -201,7 +226,137 @@ const items = [
             subCategory: "model-s",
             category: "model-s",
             stockStatus: true,
-            options: ["view-details", "best-seller"],
+            options: ["view-details", "best-seller", "Exterior"],
+          },
+          {
+            itemImg: modelSRearLiner,
+            itemImgHover: modelSRearLinerHov,
+            itemName: "Model S All-Weather Rear Cargo Liner Set",
+            itemPrice: 200,
+            subCategory: "model-s",
+            category: "model-s",
+            stockStatus: true,
+            options: ["view-details", "best-seller", "Floor Mats"],
+          },
+          {
+            itemImg: S3YLiner,
+            itemImgHover: S3YLinerHov,
+            itemName: "Model S/3/y Pet Liner",
+            itemPrice: 145,
+            subCategory: "interior",
+            category: "model-s",
+            stockStatus: true,
+            options: ["view-details", "Interior"],
+          },
+          {
+            itemImg: SDoorSills,
+            itemImgHover: SDoorSillsHov,
+            itemName: "2012-2020 | Model S Illuminated Door Sills",
+            itemPrice: 175,
+            subCategory: "interior",
+            category: "model-s",
+            stockStatus: true,
+            options: ["quick-add+", "Interior"],
+          },
+          {
+            itemImg: SXCoatHooks,
+            itemImgHover: SXCoatHooksHov,
+            itemName: "2012-2020 | Model S/X Coat Hooks",
+            itemPrice: 30,
+            subCategory: "interior",
+            category: "model-s",
+            stockStatus: true,
+            options: ["view-details", "Interior"],
+          },
+          {
+            itemImg: SunShades,
+            itemImgHover: SunShades,
+            itemName: "2012-2020 | Model S Panoramic Roof Sunshades",
+            itemPrice: 120,
+            subCategory: "interior",
+            category: "model-s",
+            stockStatus: true,
+            options: ["view-details", "Interior"],
+          },
+          {
+            itemImg: SMudFlaps,
+            itemImgHover: SMudFlapsHov,
+            itemName: "Model S Mud FLaps",
+            itemPrice: 40,
+            subCategory: "exterior",
+            category: "model-s",
+            stockStatus: true,
+            options: ["view-details", "Exterior"],
+          },
+          {
+            itemImg: SCarCover,
+            itemImgHover: SCarCoverHov,
+            itemName: "Model S Car Cover",
+            itemPrice: 325,
+            subCategory: "exterior",
+            category: "model-s",
+            stockStatus: true,
+            options: ["view-details", "Exterior"],
+          },
+          {
+            itemImg: ModelSSpoiler,
+            itemImgHover: ModelSSpoilerHov,
+            itemName: "2012-2020 | Model S Carbon Fiber Spoiler",
+            itemPrice: 1200,
+            subCategory: "exterior",
+            category: "model-s",
+            stockStatus: true,
+            options: ["view-details", "Exterior"],
+          },
+          {
+            itemImg: SBreakKit,
+            itemImgHover: SBreakKit,
+            itemName: "Model S Plaid Carbon Ceramic Brake Kit",
+            itemPrice: 20000,
+            subCategory: "Wheels and Tires",
+            category: "model-s",
+            stockStatus: true,
+            options: ["view-details", "Wheels and Tires"],
+          },
+          {
+            itemImg: STempestWheel,
+            itemImgHover: STempestWheelHov,
+            itemName: `Model S 19" Tempest Wheel and Winter Tire Package`,
+            itemPrice: 2750,
+            subCategory: "Wheels and Tires",
+            category: "model-s",
+            stockStatus: false,
+            options: ["view-details", "Wheels and Tires"],
+          },
+          {
+            itemImg: ArachnidWheel,
+            itemImgHover: ArachnidWheelHov,
+            itemName: `Model S 21" Arachnid Wheel and Winter Tire Package`,
+            itemPrice: 6000,
+            subCategory: "Wheels and Tires",
+            category: "model-s",
+            stockStatus: false,
+            options: ["view-details", "Wheels and Tires"],
+          },
+          {
+            itemImg: TireTote,
+            itemImgHover: TireToteHov,
+            itemName: `Tire Tote`,
+            itemPrice: 35,
+            subCategory: "Wheels and Tires",
+            category: "model-s",
+            stockStatus: true,
+            options: ["view-details", "Wheels and Tires"],
+          },
+          {
+            itemImg: WheelLocks,
+            itemImgHover: WheelLocksHov,
+            itemName: `Wheel Locks`,
+            itemPrice: 50,
+            subCategory: "Wheels and Tires",
+            category: "model-s",
+            stockStatus: true,
+            options: ["view-details", "Wheels and Tires"],
           },
         ],
       },
