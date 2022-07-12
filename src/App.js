197d07dfc52dbaf7ed687d303a7a24d7d6fdaa22
exionite <exionite@gmail.com>
2022-07-12 15:55:43 -0700

    footer component

--- src/App.js
+++ src/App.js
@@ -3,17 +3,14 @@ import ShopPage from "./components/shopPage/ShopPage";
 import Nav from "./components/navBar/Nav";
 import { Outlet } from "react-router-dom";
 import CardsGrid from "./components/CardsGrid";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Outlet />
-      <div className="copyright-text">
-        <h5> Tesla © 2022 </h5>
-        <h5>Privacy {'&'} Legal </h5>
-        <h5>Locations</h5>
-      </div>
+      <Footer />
     </div>
   );
 }
